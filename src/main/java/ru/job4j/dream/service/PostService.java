83f0bb805887e5bcc937e9c28143ed082ab6d1fc
Stanislav <yustas@protonmail.com>
2022-04-18 12:22:27 +0500

    2.4.1. Слоеная архитектура.

---
+++ src/main/java/ru/job4j/dream/service/PostService.java
@@ -0,0 +1,39 @@
+package ru.job4j.dream.service;
+
+import ru.job4j.dream.model.Post;
+import ru.job4j.dream.store.PostStore;
+
+import java.util.Collection;
+
+/**
+ * Верхний слой хранилища PostStore в котором находятся вакансии.
+ *
+ * @author yustas
+ * @version 1.0
+ */
+public class PostService {
+
+    private static final PostService INST = new PostService();
+
+    private final PostStore store = PostStore.instOf();
+
+    public static PostService instOf() {
+        return INST;
+    }
+
+    public Collection<Post> findAll() {
+        return store.findAll();
+    }
+
+    public void add(Post post) {
+        store.add(post);
+    }
+
+    public void update(Post post) {
+        store.update(post);
+    }
+
+    public Post findById(int id) {
+        return store.findById(id);
+    }
+}

abe18549f8cf71a3d02f28c9754e3bf1f342421f
Stanislav <yustas@protonmail.com>
2022-04-20 23:43:30 +0500

    2.5.3. Формы. Загрузка файла на сервер.

--- src/main/java/ru/job4j/dream/model/Candidate.java
+++ src/main/java/ru/job4j/dream/model/Candidate.java
@@ -15,6 +15,7 @@ public class Candidate {
     private String name;
     private String description;
     private LocalDate created;
+    private byte[] photo;
 
     public Candidate(int id, String name) {
         this.id = id;
@@ -58,6 +59,14 @@ public class Candidate {
 
     public void setCreated(LocalDate created) {
         this.created = created;
+    }
+
+    public byte[] getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(byte[] photo) {
+        this.photo = photo;
     }
 
     @Override
